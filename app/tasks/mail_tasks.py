fbf79391349eb2ec839c41dd856e4e8e48e231e2
Vlados-os1 <boatslav228@gmail.com>
2025-08-15 22:27:30 +0300

    Fix register endpoint

--- app/tasks/mail_tasks.py
+++ app/tasks/mail_tasks.py
@@ -19,7 +19,7 @@ conf = ConnectionConfig(
 
 
 @celery_app.task
-def user_mail_event(recipients, token: str):
+def user_mail_event(token: str, recipients: list[EmailStr]):
     verify_link = f"http://{settings.IP}:8000/api/verify?token={token}"
     subject = "Подтвердите ваш email"
     body_text = f"Для подтверждения перейдите по ссылке: {verify_link}"
